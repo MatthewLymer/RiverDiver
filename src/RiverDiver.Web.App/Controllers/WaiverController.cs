9002f2673a9fd73e610158c910b1f43969c34ce5
Matthew Lymer <matthew.lymer@tripstack.com>
2020-05-21 22:50:05 -0400

    Add PDF library

--- src/RiverDiver.Web.App/Controllers/WaiverController.cs
+++ src/RiverDiver.Web.App/Controllers/WaiverController.cs
@@ -1,5 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Threading;
+using Microsoft.AspNetCore.Mvc;
 using RiverDiver.Web.App.Models;
+using Spire.Pdf;
+using Spire.Pdf.HtmlConverter.Qt;
 
 namespace RiverDiver.Web.App.Controllers
 {
@@ -14,7 +20,20 @@ namespace RiverDiver.Web.App.Controllers
         [HttpPost]
         public IActionResult Index(WaiverModel waiver)
         {
-            return View("Index", waiver);
+            if (ModelState.IsValid)
+            {
+                var doc = new PdfDocument();
+                
+                var memoryStream = new MemoryStream();
+                
+                doc.SaveToStream(memoryStream);
+                
+                memoryStream.Position = 0;
+
+                return File(memoryStream, "application/pdf");
+            }
+            
+            return View(waiver);
         }
     }
 }
